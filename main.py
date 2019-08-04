67be9c242b468654e7ebedc22a21bdfb19181371
Stephen <stephen@Stephens-MBP.hsd1.il.comcast.net>
2019-08-04 13:26:04 -0500

    Update main.py
    Fixed a bug where posts without comments would get an index error when attempting to download the photos from a public profile
    
    Fixed a char encoding bug (Issue #6)

--- main.py
+++ main.py
@@ -43,60 +43,63 @@ class InstagramOSINT:
                  'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
                  'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
                  'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
-
-        # Username = username passed into __init__
+                 
         self.username = username
         # Make the directory that we are putting the files into
         self.make_directory()
-        print(colors.OKGREEN + f"[*] Starting Scan on {username}" + colors.ENDC)
+        print(colors.OKGREEN + f"[*] Starting Scan on {self.username}" + colors.ENDC)
         # Get the html data with the requests module
         r = requests.get(f'http://instagram.com/{self.username}', headers={'User-Agent': random.choice(self.useragents)})
         soup = BeautifulSoup(r.text, 'html.parser')
+        # To prevent a unicode error, we need the following line...
+        soup.encode('utf-8')
         # Find the tags that hold the data we want to parse
         general_data = soup.find_all('meta', attrs={'property': 'og:description'})
         more_data = soup.find_all('script', attrs={'type': 'text/javascript'})
         description = soup.find('script', attrs={'type': 'application/ld+json'})
         # Try to parse the content -- if it fails then the program exits
         try:
-            text = general_data[0].get('content').split()
-            description = json.loads(description.get_text())
-            profile_meta = json.loads(more_data[3].get_text()[21:].strip(';'))
+            self.text = general_data[0].get('content').split()
+            # This is the profile description data
+            self.description = json.loads(description.get_text())
+            # This is the javascript json that is passed into json.loads()
+            self.profile_meta = json.loads(more_data[3].get_text()[21:].strip(';'))
 
         except:
-            print(colors.FAIL + f"Username {username} not found" + colors.ENDC)
+            print(colors.FAIL + f"Username {self.username} not found" + colors.ENDC)
             sys.exit()
-        self.profile_data = {"Username": profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['username'],
-                             "Profile name": description['name'],
-                             "URL": description['mainEntityofPage']['@id'],
-                             "Followers": text[0], "Following": text[2], "Posts": text[4],
+        self.profile_data = {"Username": self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['username'],
+                             "Profile name": self.description['name'],
+                             "URL": self.description['mainEntityofPage']['@id'],
+                             "Followers": self.text[0], "Following": self.text[2], "Posts": self.text[4],
                              "Bio": str(
-                                 profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['biography']),
-                             "profile_pic_url": str(profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
+                                 self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['biography']),
+                             "profile_pic_url": str(self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
                                                         'profile_pic_url_hd']),
                              "is_business_account": str(
-                                 profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
+                                 self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
                                      'is_business_account']),
-                             "connected_to_fb": str(profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
+                             "connected_to_fb": str(self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
                                                         'connected_fb_page']),
                              "externalurl": str(
-                                 profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['external_url']),
-                             "joined_recently": str(profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
+                                 self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['external_url']),
+                             "joined_recently": str(self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
                                                         'is_joined_recently']),
                              "business_category_name": str(
-                                 profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
+                                 self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user'][
                                      'business_category_name']),
                              "is_private": str(
-                                 profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['is_private']),
+                                 self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['is_private']),
                              "is_verified": str(
-                                 profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['is_verified'])}
+                                 self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['is_verified'])}
 
         # Tries to scrape posts if it is a public profile
         self.save_data()
         if downloadPhotos == True:
-            self.scrape_posts(profile_meta)
+            self.scrape_posts()
         self.print_data()
 
-    def scrape_posts(self, profile_meta: str):
+    def scrape_posts(self):
         """Scrapes all posts and downloads thumbnails when necessary
         :return: none
         :param: none
@@ -106,16 +109,26 @@ class InstagramOSINT:
         else:
             print("[*]Getting Photos")
             posts = {}
-            for index, post in enumerate(profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges']):
+            for index, post in enumerate(self.profile_meta['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges']):
                 os.mkdir(str(index))
-                posts[index] = {"Caption": str(post['node']['edge_media_to_caption']['edges'][0]['node']['text']),
-                                "Number of Comments": str(post['node']['edge_media_to_comment']['count']),
-                                "Comments Disabled": str(post['node']['comments_disabled']),
-                                "Taken At Timestamp": str(post['node']['taken_at_timestamp']),
-                                "Number of Likes": str(post['node']['edge_liked_by']['count']),
-                                "Location": str(post['node']['location']),
-                                "Accessability Caption": str(post['node']['accessibility_caption'])
-                                }
+                try:
+                    posts[index] = {"Caption": str(post['node']['edge_media_to_caption']['edges'][0]['node']['text']),
+                                    "Number of Comments": str(post['node']['edge_media_to_comment']['count']),
+                                    "Comments Disabled": str(post['node']['comments_disabled']),
+                                    "Taken At Timestamp": str(post['node']['taken_at_timestamp']),
+                                    "Number of Likes": str(post['node']['edge_liked_by']['count']),
+                                    "Location": str(post['node']['location']),
+                                    "Accessability Caption": str(post['node']['accessibility_caption'])
+                                    }
+                except IndexError:
+                    posts[index] = {"Caption": 'No Caption on this post',
+                                    "Number of Comments": str(post['node']['edge_media_to_comment']['count']),
+                                    "Comments Disabled": str(post['node']['comments_disabled']),
+                                    "Taken At Timestamp": str(post['node']['taken_at_timestamp']),
+                                    "Number of Likes": str(post['node']['edge_liked_by']['count']),
+                                    "Location": str(post['node']['location']),
+                                    "Accessability Caption": str(post['node']['accessibility_caption'])
+                                    }
 
                 # Downloads the thumbnails of the post
                 # Picture is just an int index of the url in the list
@@ -139,6 +152,8 @@ class InstagramOSINT:
             os.chdir(self.username)
         except FileExistsError:
             num = 0
+            # This is a loop to keep trying to make a new directory if a scan has already
+            # been done on a profile and that directory exists
             while os.path.exists(self.username):
                 num += 1
                 try:
@@ -167,7 +182,7 @@ class InstagramOSINT:
         print(colors.HEADER + "---------------------------------------------" + colors.ENDC)
         print(colors.OKGREEN + f"Results: scan for {self.profile_data['Username']} on instagram" + colors.ENDC)
         for key, value in self.profile_data.items():
-            print(key + ':' + value)
+            print(key + ': ' + value)
 
     def download_profile_picture(self):
         """Downloads the profile pic and saves it to the directory
